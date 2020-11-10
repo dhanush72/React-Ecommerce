bcfb5508fb89fbaff9182a01641edea82aa10cef
Dhanush <dhanuraj7258@gmail.com>
2020-11-10 10:56:58 +0530

    menu items added

---
+++ src/components/directory/directory.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./directory.scss";
+import data from "./directory-data";
+import MenuItem from "../../components/menu-item/menu-item";
+
+const Directory = () => {
+  return (
+    <div className="directory-menu">
+      {data.map((section, index) => {
+        return <MenuItem key={index} {...section} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
