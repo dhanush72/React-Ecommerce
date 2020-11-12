4645411faa420cbc41294008a622581b912041f8
Dhanush <dhanuraj7258@gmail.com>
2020-11-12 10:53:54 +0530

    showing items in cart

--- src/components/cart/cart-dropdown.js
+++ src/components/cart/cart-dropdown.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
+import CartItem from "../cart-item/cart-item";
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        <CustomButton>CHECKOUT</CustomButton>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
       </div>
+      <CustomButton>CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default Cart;
+const mapDispatchToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapDispatchToProps)(Cart);
