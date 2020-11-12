9f8e7ebf314c9f45894f0260a6d6fb8b216023fa
Dhanush <dhanuraj7258@gmail.com>
2020-11-12 11:23:35 +0530

    fixed re-render after adding new items to cart

--- src/components/cart/cart-dropdown.js
+++ src/components/cart/cart-dropdown.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
 import CartItem from "../cart-item/cart-item";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 const Cart = ({ cartItems }) => {
   return (
@@ -16,8 +17,8 @@ const Cart = ({ cartItems }) => {
   );
 };
 
-const mapDispatchToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapDispatchToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapDispatchToProps)(Cart);
