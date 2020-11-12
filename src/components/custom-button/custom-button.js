ba8924ac612f19bf0fa49a786dce52d1322eb0e8
Dhanush <dhanuraj7258@gmail.com>
2020-11-12 10:10:09 +0530

    added onhover cart button on item

--- src/components/custom-button/custom-button.js
+++ src/components/custom-button/custom-button.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const CustomButton = ({ children, isGoogleSignIn, ...others }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...others }) => {
   return (
     <button
-      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+      className={`${inverted ? "inverted" : ""} ${
+        isGoogleSignIn ? "google-sign-in" : ""
+      } custom-button`}
       {...others}
     >
       {children}
